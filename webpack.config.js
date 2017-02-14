204ee88df8dd678ca37459b2c00600d078daf9e2
nbb3210 <18627992557@163.com>
2017-02-14 12:30:12 +0800

    环境搭建

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack')
+var path = require('path')
+
+module.exports = {
+  entry: {
+    app: './src/app.js'
+  },
+  output: {
+    filename: 'public/build/bundle.js',
+    sourceMapFilename: 'public/build/bundle.map'
+  },
+  devtool: '#source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+}
